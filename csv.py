0c23c7134aed66c96a059395a2774b2a059845c7
Nisseya <yassinehadi78190@gmail.com>
2025-02-03 21:51:25 +0100

    Commentaires

--- csv.py
+++ csv.py
@@ -1,5 +1,12 @@
 class csv_data:
     def __init__(self,file_path,sep=','):
+        """Crée l'objet cd (moche il faut changer le nom psk cd ca fait commande terminal et j'aime pas)
+        Il a plusieurs attributs sympas: 
+        data -> data sous forme de dictionnaire avec les colonnes
+        lines-> data sous forme de lignes, pas sur que j'aime bien, peut être à retirer si ca sert à rien honnêtement
+        columns -> liste des colonnes
+        dtypes -> les types de chaque colonne
+        """
         self.lines=[]
         self.columns = []
         #Lecture du csv ligne par ligne et stockage
@@ -9,6 +16,7 @@ class csv_data:
             for line in lines[1:]:
                 self.lines.append(line.strip().split(sep))
     
+        #Data sous forme de colonnes
         self.data={}
         map_index={}
         for index,column in enumerate(self.columns):
@@ -24,16 +32,18 @@ class csv_data:
         self.dtypes = {}
         for column in self.columns:
             #part du principe que le csv est propre dès le début
+            #à changer si les csv peuvent être sales mais flemme un peu psk 
+            # c'est long et pas opti
             self.dtypes[column] = self.list_type(column)
 
     def list_type(self,column):
         # à modifier si le csv peut avoir des mixed data
-        return type(self.data[column])
+        return type(self.data[column][0])
 
     
 
 
 cd=csv_data("D:\Downloads\iris.csv",sep=";")
 print(cd.columns)
-print(cd.data)
+print(cd.dtypes)
 
